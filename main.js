e8cb95f6ddac11b3c769b4165b91d12d4e97409d
Samara-rbd <maiareismaia@gmail.com>
2021-12-22 15:18:13 -0300

    feart:main finalidado

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function changeSocialMediaLinks() {
     for (let li of socialLinks.children){
         const social = li.getAttribute('class')   
       
-        li.children[0].href = `https:${social}.com/${LinksSocialMedia [socials]}`
+        li.children[0].href = `https:${social}.com/${LinksSocialMedia [social]}`
     }
 }
 
@@ -20,7 +20,15 @@ function getGitHubProfileInfos(){
     const url = `https://api.github.com/users/${LinksSocialMedia.github}`
     
     fetch(url)
-    
+    .then(response =>  response.json())
+    .then( data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        UserPhoto.src = data.avatar_url
+        userLogin.textContent = data.login
+    })
 }
 
 getGitHubProfileInfos()
+
