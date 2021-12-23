3147e21779056cb35ffa9d24632b2ec556289d82
Samara-rbd <maiareismaia@gmail.com>
2021-12-23 17:54:28 -0300

    pequenas alteracçoes

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function changeSocialMediaLinks() {
     for (let li of socialLinks.children){
         const social = li.getAttribute('class')   
       
-        li.children[0].href = `https:${social}.com/${LinksSocialMedia [social]}`
+        li.children[0].href = `https://${social}.com/${LinksSocialMedia [social]}`
     }
 }
 
